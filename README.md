a6bad09adb34d612388c9a122e8369d6205c3f37
rbonifacio <rbonifacio123@gmail.com>
2024-04-04 07:49:20 -0300

    removing futures

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-## sbt project compiled with Scala 3
+## Futures
 
-### Usage
+```
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
-This is a normal sbt project. You can compile code with `sbt compile`, run it with `sbt run`, and `sbt console` will start a Scala 3 REPL.
 
-For more information on the sbt-dotty plugin, see the
-[scala3-example-project](https://github.com/scala/scala3-example-project/blob/main/README.md).
+def longRunningAlgorithm() =
+  Thread.sleep(10_000)
+  42
+  
+val eventualInt = Future(longRunningAlgorithm())  
+``` 
--- src/main/scala/br/unb/cic/wc/WordCount.scala
+++ src/main/scala/br/unb/cic/wc/WordCount.scala
@@ -14,18 +14,6 @@ def split(lines: List[String], n: Integer = 100): List[List[String]] =
     f :: split(s, n)
 
 
-def mapF[a](f : List[String] => a)(chuncks: List[List[String]]): List[Future[a]] = chuncks.map(list => Future { f(list) })
+def mapF[a](f : List[String] => a)(chuncks: List[List[String]]): List[a] = chuncks.map(list => f(list))
 
-def reduceF[a,b](base : b)(op : (b, a) => b)(futures : List[Future[a]]): b = {
-  print(futures.length)
-  val f = Future.sequence(futures)
-  var res : scala.collection.mutable.Set[b] = scala.collection.mutable.Set.empty
-  f.onComplete {
-    case Success(l) => res.addOne(l.foldLeft(base)(op))
-    case Failure(e) => {
-      print("foo")
-      throw new Exception("foo")
-    }
-  }
-  res.iterator.next()
-}
+def reduceF[a,b](base : b)(op : (b, a) => b)(futures : List[a]): b = futures.foldLeft(base)(op)
