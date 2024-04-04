3d6ab4c19633f0eeaaa769778d5c7c90b1c66315
rbonifacio <rbonifacio123@gmail.com>
2024-04-04 08:11:35 -0300

    versao intermediaria

--- src/main/scala/br/unb/cic/wc/WordCount.scala
+++ src/main/scala/br/unb/cic/wc/WordCount.scala
@@ -17,15 +17,23 @@ def split(lines: List[String], n: Integer = 100): List[List[String]] =
 def mapF[a](f : List[String] => a)(chuncks: List[List[String]]): List[Future[a]] = chuncks.map(list => Future { f(list) })
 
 def reduceF[a,b](base : b)(op : (b, a) => b)(futures : List[Future[a]]): b = {
-  print(futures.length)
-  val f = Future.sequence(futures)
-  var res : scala.collection.mutable.Set[b] = scala.collection.mutable.Set.empty
-  f.onComplete {
-    case Success(l) => res.addOne(l.foldLeft(base)(op))
-    case Failure(e) => {
-      print("foo")
-      throw new Exception("foo")
+  val buf = scala.collection.mutable.ListBuffer.empty[a]
+
+  val res =
+    for future <- futures buf += future
+    yield (buf.foldLeft(base)(op))
+
+  var temp = False
+  res.onComplete {
+    case Success(v) => temp = True 
+    case Failure(e) => temp = False
+  }
+
+  if(temp) {
+    match value {
+      case Some(Success(v)) => v
+      case _ => throw new RuntimeException("not expecting")  
     }
   }
-  res.iterator.next()
+  else throw new RuntimeException("error!!!!")
 }
