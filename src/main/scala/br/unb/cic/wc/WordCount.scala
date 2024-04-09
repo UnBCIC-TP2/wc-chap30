9a125cbb384b2d619bd59d1d4ce668a6e732a75e
rbonifacio <rbonifacio123@gmail.com>
2024-04-09 07:49:12 -0300

    generalization of map and fold

--- src/main/scala/br/unb/cic/wc/WordCount.scala
+++ src/main/scala/br/unb/cic/wc/WordCount.scala
@@ -7,13 +7,13 @@ import scala.util.{Failure, Success}
 import scala.compiletime.uninitialized
 type line = String
 
-def split(lines: List[String], n: Integer = 100): List[List[String]] =
+def split[a](lines: List[a], n: Integer = 100): List[List[a]] =
   if lines == List() then List()
   else
     val (f, s) = lines.splitAt(n)
     f :: split(s, n)
 
 
-def mapF[a](f : List[String] => a)(chuncks: List[List[String]]): List[a] = chuncks.map(list => f(list))
+def mapF[a,b](f : List[a] => b)(chuncks: List[List[a]]): List[b] = chuncks.map(list => f(list))
 
-def reduceF[a,b](base : b)(op : (b, a) => b)(futures : List[a]): b = futures.foldLeft(base)(op)
+def reduceF[a,b](base : b)(op : (b, a) => b)(list : List[a]): b = list.foldLeft(base)(op)
